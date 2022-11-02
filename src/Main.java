561de29bb5206d06862285e9bf2662997a40182d
Альвина <alvina-b@yandex.ru>
2022-11-02 18:48:52 +0600

    вторая часть.3 задачи переделанные с else

--- src/Main.java
+++ src/Main.java
@@ -24,13 +24,30 @@ public class Main {
         //Задача 3
         System.out.println("  Задача 3");
 
-     int seatsOnTheBus = 53;
-     if (seatsOnTheBus < 60) {
+     int seatsOnTheBus = 83;
+int seating = 60;
+int capacity = 102;
+     if (seatsOnTheBus < seating) {
          System.out.println("В автобусе есть места сидячие и стоячие.");
-     } else if (seatsOnTheBus >= 60 && seatsOnTheBus < 102) {
+     } else if (seatsOnTheBus >= seating && seatsOnTheBus < capacity) {
          System.out.println("В автобусе есть только стоячие места.");
      } else {
          System.out.println("К сожалению, в автобусе нет мест.");
      }
+
+       //Задание 1.блок 3.
+        System.out.println("  Задание 1.блок 3.");
+
+     int institution = 16;
+     if (institution >= 2 && institution < 6) {
+         System.out.println("Если возраст человека от 2 до 6, то ему нужно ходить в детский сад");
+     } else if (institution >= 7 && institution < 18) {
+         System.out.println("Если человеку от 7 до 18, то ему нужно ходить в школу");
+     } else if (institution >= 18 && institution < 24) {
+         System.out.println("Если человеку от 18 до 24, то его место в университете");
+     } else {
+         System.out.println("Если человеку больше 24, то ему нужно ходить на работу");
+     }
+
     }
 }
