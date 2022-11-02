6f8d5d8734e59817c5d1eeac51d9b232705ffa23
Альвина <alvina-b@yandex.ru>
2022-11-02 22:00:48 +0600

    оптимизировала после разбора, убрала лишние добавленные переменные

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
          System.out.println("Ты ходишь в школу");
      } else if (humanStatus >= 18 && humanStatus < 24) {
          System.out.println("Ты закончил школу и можешь отправляться в универитет.");
-     } else {
+     } else if (humanStatus >= 24) {
          System.out.println("Ты окончил университет и пора искать первую работу.");
      }
 
@@ -39,34 +39,24 @@ int capacity = 102;
         System.out.println("  Задание 1.блок 3.");
 
      int ageMan = 21;
-     int childJunior = 2;
-     int childOlder = 6;
-     int schoolboyJunior = 7;
-     int schoolboyOlder = 18;
-     int adult = 24;
-     if (ageMan >= childJunior && ageMan < childOlder) {
-         System.out.println("Если возраст человека от 2 до 6, то ему нужно ходить в детский сад");
-     } else if (ageMan >= schoolboyJunior && ageMan < schoolboyOlder) {
-         System.out.println("Если человеку от 7 до 18, то ему нужно ходить в школу");
-     } else if (ageMan >= schoolboyOlder && ageMan < adult) {
-         System.out.println("Если человеку от 18 до 24, то его место в университете");
-     } else {
-         System.out.println("Если человеку больше 24, то ему нужно ходить на работу");
+          if (ageMan >= 2 && ageMan < 6) {
+         System.out.println("Если возраст человека " + ageMan + ", то ему нужно ходить в детский сад");
+     } else if (ageMan >= 7 && ageMan < 18) {
+         System.out.println("Если человеку " + ageMan + ", то ему нужно ходить в школу");
+     } else if (ageMan >= 18 && ageMan < 24) {
+         System.out.println("Если человеку " + ageMan + ", то его место в университете");
+     } else if (ageMan >=24) {
+         System.out.println("Если человеку " + ageMan + ", то ему нужно ходить на работу");
      }
 
      //Задание 2. блок 3
         System.out.println("  Задание 2. блок 3");
-     int ageKid = 18;
-     int kidJuniorLimit = 5;
-     int kidOlderLimit = 14;
-     int maintainer = 0;
-     if (ageKid < kidJuniorLimit) {
+     int ageKid = 15;
+         if (ageKid < 5) {
          System.out.println("Ты не можешь кататься на аттракционе.");
-     } else if (ageKid >= kidJuniorLimit && ageKid < kidOlderLimit && maintainer >= 1 ) {
+     } else if (ageKid < 14) {
          System.out.println("Ты можешь кататься в сопровождении взрослого.");
-     } else if (ageKid >= kidJuniorLimit && ageKid < kidOlderLimit && maintainer < 1 ) {
-         System.out.println("Ты не можешь кататься, так как нет взрослого сопровождающего");
-     } else if (ageKid >= kidOlderLimit) {
+          } else {
             System.out.println("Ты можешь кататься без сопровождения взрослого");
         }
 
