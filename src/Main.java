6f88ddb7fa03f5f9a9305475eed264fdcc7a8786
Альвина <alvina-b@yandex.ru>
2022-11-02 20:05:26 +0600

    задачи. блок 3 + исправление задачи 3 блока2

--- src/Main.java
+++ src/Main.java
@@ -38,16 +38,49 @@ int capacity = 102;
        //Задание 1.блок 3.
         System.out.println("  Задание 1.блок 3.");
 
-     int institution = 16;
-     if (institution >= 2 && institution < 6) {
+     int ageMan = 21;
+     int childJunior = 2;
+     int childOlder = 6;
+     int schoolboyJunior = 7;
+     int schoolboyOlder = 18;
+     int adult = 24;
+     if (ageMan >= childJunior && ageMan < childOlder) {
          System.out.println("Если возраст человека от 2 до 6, то ему нужно ходить в детский сад");
-     } else if (institution >= 7 && institution < 18) {
+     } else if (ageMan >= schoolboyJunior && ageMan < schoolboyOlder) {
          System.out.println("Если человеку от 7 до 18, то ему нужно ходить в школу");
-     } else if (institution >= 18 && institution < 24) {
+     } else if (ageMan >= schoolboyOlder && ageMan < adult) {
          System.out.println("Если человеку от 18 до 24, то его место в университете");
      } else {
          System.out.println("Если человеку больше 24, то ему нужно ходить на работу");
      }
 
+     //Задание 2. блок 3
+        System.out.println("  Задание 2. блок 3");
+     int ageKid = 18;
+     int kidJuniorLimit = 5;
+     int kidOlderLimit = 14;
+     int maintainer = 0;
+     if (ageKid < kidJuniorLimit) {
+         System.out.println("Ты не можешь кататься на аттракционе.");
+     } else if (ageKid >= kidJuniorLimit && ageKid < kidOlderLimit && maintainer >= 1 ) {
+         System.out.println("Ты можешь кататься в сопровождении взрослого.");
+     } else if (ageKid >= kidJuniorLimit && ageKid < kidOlderLimit && maintainer < 1 ) {
+         System.out.println("Ты не можешь кататься, так как нет взрослого сопровождающего");
+     } else if (ageKid >= kidOlderLimit) {
+            System.out.println("Ты можешь кататься без сопровождения взрослого");
+        }
+
+        //Задание 2. блок 3
+        System.out.println("  Задание 2. блок 3");
+     int one = 2;
+     int two = 3;
+     int three = 1;
+     if (one > two && one > three) {
+         System.out.println("Число ONE - самое большое");
+     } else if (two > one && two > three) {
+         System.out.println("Число TWO - самое большое");
+     } else if (three > two && three > one) {
+         System.out.println("Число THREE - самое большое");
+     }
     }
 }
